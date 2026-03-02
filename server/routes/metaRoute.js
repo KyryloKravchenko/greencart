dbb7fc5f11902694d43e4d3f6e3637de4ec55259
KyryloKravchenko <kakravchenko.fitm22@kubg.edu.ua>
2026-03-02 20:34:25 +0200

    Delete server directory

--- server/routes/metaRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUsdToUahRate } from "../controllers/metaController.js";
-
-const metaRouter = express.Router();
-
-metaRouter.get("/exchange-rate", getUsdToUahRate);
-
-export default metaRouter;
